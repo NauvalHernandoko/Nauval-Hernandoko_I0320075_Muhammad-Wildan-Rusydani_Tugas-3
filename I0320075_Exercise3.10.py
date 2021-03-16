75ae98ec43cd03b381dbdeb35292b2bbfae9c427
Nauval Hernandoko <nauvalhernadoko@gmail.com>
2021-03-16 15:17:23 +0700

    Exercise3.10 tugas 3

---
+++ I0320075_Exercise3.10.py
@@ -0,0 +1,9 @@
+tup = ['fisika','kimia', 1999, 2009]
+print(tup)
+
+#hapus tup dengan statement del
+del tup
+
+#buat tuple baru dengan element yang diinginkan
+tup = ['kalkulus','meterial_teknik',1998,2021]
+print("setelah menghapus tuple:",tup)
