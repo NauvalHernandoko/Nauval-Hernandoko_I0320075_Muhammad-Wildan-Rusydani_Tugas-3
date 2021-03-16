7e97d8bde6e8e9e629767ed3281af16f4e7ec053
Nauval Hernandoko <nauvalhernadoko@gmail.com>
2021-03-16 15:16:48 +0700

    Exercise3.7 tugas 3

---
+++ I0320075_Exercise3.7.py
@@ -0,0 +1,8 @@
+dict = {'Name':'Nando','Age':19, 'semester':'third'}
+
+del dict['Name'] #hapus entri dengan key 'Name'
+dict.clear  #hapus semua entri di dict
+del dict    #hapus dictionary yang sudah ada
+
+print("dict['Age']:" ,dict['Age'])
+print("dict['school]:" ,dict['school'])
