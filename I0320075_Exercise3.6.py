a974c959cc56fbfa45ce01381164859d25d0d3a4
Nauval Hernandoko <nauvalhernadoko@gmail.com>
2021-03-16 15:16:36 +0700

    Exercise3.6 tugas 3

---
+++ I0320075_Exercise3.6.py
@@ -0,0 +1,6 @@
+dict = {'Name':'Nando','Age':19, 'semester':'third'}
+dict['Age'] = 18 #Mengubah entri yang sudah ada
+dict['school'] = 'DPS school'
+
+print("dict['Age']:" ,dict['Age'])
+print("dict['school]:" ,dict['school'])
